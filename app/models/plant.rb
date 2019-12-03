83061959dfcc3416126daf35ebff423324cddd1d
sarah wang <sarahwang47@gmail.com>
2019-12-03 13:01:32 +0800

    favoritable

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,4 @@
 class Plant < ApplicationRecord
   belongs_to :user
+  acts_as_favoritable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :plants, dependent: :destroy
+  acts_as_favoritor
 end
