44f0163151fd15a5174e7615c73c8bee348303d8
kaki li <kaki.noodles@gmail.com>
2019-12-03 19:35:09 +0800

    fixed controller

--- app/controllers/api/v1/plant_libraries_controller.rb
+++ app/controllers/api/v1/plant_libraries_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::PlantLibrariesController < Api::V1::BaseController
 
  def index
-   if params[:query].nil or params[:query] == ""
+   if params[:query].nil? or params[:query] == ""
      @plant_libraries = PlantLibrary.all
    else
      @plant_libraries = PlantLibrary.search_by_name_and_latin_name(params[:query])
