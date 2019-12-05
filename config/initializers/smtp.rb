13e2b5be89366c8d8602b1832785fd26968a0f10
sarah wang <sarahwang47@gmail.com>
2019-12-05 19:59:51 +0800

    commit

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
   domain: 'https://plantopia.wogengapp.cn',
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
+  address:        "smtp.sendgrid.net",
+  port:            587,
+  authentication: :plain,
+  user_name:      'WZz2ODyGTqmnaDr0YrZ-Yw',
+  password:       ENV['SENDGRID_API_KEY']
 }
+
