e341ac55fa8bbd4a539a243de6e198dfbabe8a0d
kaki li <kaki.noodles@gmail.com>
2019-12-05 15:11:04 +0800

    half chatroom

--- app/controllers/api/v1/plant_chats_controller.rb
+++ app/controllers/api/v1/plant_chats_controller.rb
@@ -4,5 +4,27 @@ class Api::V1::PlantChatsController < Api::V1::BaseController
     @plant_chat = PlantChat.find(params[:id])
   end
 
+
+  def create
+    @plant_chat = PlantChat.new(params_plant_chat)
+     if @plant_chat.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+    private
+
+  def params_plant_chat
+    params.require(:plant_chat).permit(:user_id, :plant_id)
+  end
+
+
+  def render_error
+    render json: { errors: @message.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
 end
 
