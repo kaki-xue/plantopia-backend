aa2ec3b12d11653c6abf2b5293dbe82e9058c2af
kaki li <kaki.noodles@gmail.com>
2019-12-05 19:21:51 +0800

    changed routes. Delete and update now working

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,6 @@
 class Plant < ApplicationRecord
   belongs_to :user
   belongs_to :plant_library, class_name: "Plant",optional:true
+  has_one :plant_chat, dependent: :destroy
   acts_as_favoritable
 end
