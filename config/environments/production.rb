7937d22cf56b5e7cfefba57e603ed29f4493b6a6
sarah wang <sarahwang47@gmail.com>
2019-12-06 20:20:47 +0800

    gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,10 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "https://plantopia.wogengapp.cn" }
+
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
