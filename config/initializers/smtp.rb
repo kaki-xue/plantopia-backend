18ae7961656fcaa8a744ca5bf115a61012cda15e
sarah wang <sarahwang47@gmail.com>
2019-12-06 19:21:59 +0800

    delete grid

--- config/initializers/smtp.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  domain: 'https://plantopia.wogengapp.cn',
-  address:        "smtp.sendgrid.net",
-  port:            587,
-  authentication: :plain,
-  user_name:      'WZz2ODyGTqmnaDr0YrZ-Yw',
-  password:       ENV['SENDGRID_API_KEY']
-}
-
