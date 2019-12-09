9df72ad858921618d20982a749bcdeba9ddb1d47
Tom Hill <tom.hill991@gmail.com>
2019-12-09 15:30:51 +0800

    watering schedule is timed again based off of when the user waters the plant. Add logic to stop user from watering when it is unnecessary

--- app/controllers/api/v1/plant_chats_controller.rb
+++ app/controllers/api/v1/plant_chats_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::PlantChatsController < Api::V1::BaseController
+
 skip_before_action :verify_authenticity_token
+
   def show
     @plant_chat = PlantChat.find(params[:id])
   end
-
 
   def create
     @plant_chat = PlantChat.where(user_id: params_plant_chat[:user_id], plant_id: params_plant_chat[:plant_id])
@@ -19,17 +20,15 @@ skip_before_action :verify_authenticity_token
     end
   end
 
-    private
+  private
 
   def params_plant_chat
     params.require(:plant_chat).permit(:user_id, :plant_id)
   end
 
-
   def render_error
     render json: { errors: @plant_chat.errors.full_messages },
       status: :unprocessable_entity
   end
-
 end
 
