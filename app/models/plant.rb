a101890154110c778afbcfbc2f29e9feb3a1f4b5
kaki li <kaki.noodles@gmail.com>
2019-12-10 18:49:31 +0800

    fixed name of update and my plant page on backend

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,7 @@
 class Plant < ApplicationRecord
   belongs_to :user
-  belongs_to :plant_library, class_name: "Plant",optional:true
+  # belongs_to :plant_library, class_name: "Plant",optional:true
+  belongs_to :plant_library, optional:true
   has_one :plant_chat, dependent: :destroy
   has_many :messages, through: :plant_chat
 end
--- app/views/api/v1/plants/show.json.jbuilder
+++ app/views/api/v1/plants/show.json.jbuilder
@@ -1,2 +1,7 @@
 json.extract! @plant, :id, :nickname, :image, :water_frequency, :description, :name, :plant_library_id, :user_id
 
+unless @plant.plant_library_id.nil?
+
+  json.plant_library @plant.plant_library, :latin_name, :description, :family, :average_height, :light_preference, :soil, :flowering, :watering, :careinfo
+
+end
